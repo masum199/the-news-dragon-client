a951e63d55842a55404e5c5ee0d7ee9962665c7d
unknown <24abdullahalmasum@gmail.com>
2023-04-29 00:18:25 -0700

    added

---
+++ src/pages/News/News/News.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import { Card } from 'react-bootstrap/esm';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+
+const News = () => {
+    const news = useLoaderData()
+    const { _id, title, details, image_url,category_id} = news
+    return (
+        <Card>
+        <Card.Img variant="top" src={image_url}/>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {details}
+          </Card.Text>
+        <Link to={`/category/${category_id}`}><Button variant="danger"> <FaArrowLeft></FaArrowLeft>All News in this category</Button></Link>
+        </Card.Body>
+      </Card>
+    );
+};
+
+export default News;
